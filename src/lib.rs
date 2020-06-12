cdb0423e2abd5c08c7e04e35148f0523cc3170db
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-06-12 15:53:13 +0200

    Docs pt.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,220 @@
 #![recursion_limit = "256"]
 use std::marker::PhantomData;
 use trait_eval::*;
+pub use trait_eval::Eval;
 
+#[doc(hidden)]
 pub struct Empty {}
 
+#[doc(hidden)]
 pub struct Node<V, N> {
     _val: PhantomData<V>,
     _next: PhantomData<N>,
 }
 
+#[doc(hidden)]
 pub struct Stop<N> {
     _next: PhantomData<N>,
 }
 
 macro_rules! pub_trait {
-    ($($name:ident),*) => {
+    ($($(#[$meta:meta])* $name:ident),*) => {
         $(
+            $(#[$meta])*
             pub trait $name {
                 type Result;
             }
         )*
     }
 }
+
+pub_trait!(
+    /// Remove the top element
+    /// # Examples
+    /// Drop 2 from the stack:
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     1 2 drop
+    ///     return type Out as top
+    /// );
+    /// assert_eq!(Out::eval(), 1);
+    /// ```
+    drop,
+
+    /// Duplicate the top element
+    /// # Examples
+    /// Duplicate 2 and add it to itself:
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     2 dup +
+    ///     return type Out as top
+    /// );
+    /// assert_eq!(Out::eval(), 4);
+    /// ```
+    dup,
+
+    /// Swap two top elements
+    /// # Examples
+    /// Swap 1 and 2 and subtract
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     1 2 swap -
+    ///     return type Out as top
+    /// );
+    /// assert_eq!(Out::eval(), 1);
+    /// ```
+    swap,
+
+    /// Rotate three top elements
+    /// # Examples
+    /// Rotate 1 2 3 -> 2 3 1 -> 3 1 2
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     1 2 3 rot rot
+    ///     return type Out as top
+    /// );
+    /// assert_eq!(Out::eval(), 2);
+    /// ```
+    rot,
+
+    /// Get the top element
+    /// # Examples
+    /// WARNING! This effectively discards the stack, so it can only be used with the `return`
+    /// statement
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     10
+    ///     return type Out1 as top
+    ///     1 + top
+    ///     return type Out2
+    /// );
+    /// type Out3 = forth!(9 top return);
+    /// assert_eq!(Out1::eval(), 10);
+    /// assert_eq!(Out2::eval(), 11);
+    /// assert_eq!(Out3::eval(), 9);
+    /// ```
+    top,
+
+    /// ( if / else / then ) conditional expression
+    ///
+    /// ifs can be nested and can be used both inside and outside of a function. `else` clause is
+    /// optional
+    /// # Examples
+    /// Return 9 if 10 is less than 1, else return 9
+    /// ```
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///     10 1 < if 5 else 9 then
+    ///     return type Out as top
+    /// );
+    /// assert_eq!(Out::eval(), 9);
+    /// ```
+    /// FizzBuzz (of course), there are no strings or chars in fortraith so Fizz = true, Buzz =
+    /// false and FizzBuzz = 0
+    /// ```
+    /// # #![recursion_limit="256"]
+    /// # #[macro_use] extern crate fortraith;
+    /// # use fortraith::*;
+    /// forth!(
+    ///    : FizzBuzz
+    ///    dup 3 % 0 = if
+    ///        5 % 0 = if
+    ///            0 (FizzBuzz)
+    ///        else
+    ///            true (Fizz)
+    ///        then
+    ///    else
+    ///        dup 5 % 0 = if
+    ///            drop false (Buzz)
+    ///        then
+    ///    then ;
+    ///    1 FizzBuzz return type Out1 as top
+    ///    2 FizzBuzz return type Out2 as top
+    ///    3 FizzBuzz return type Out3 as top
+    ///    4 FizzBuzz return type Out4 as top
+    ///    5 FizzBuzz return type Out5 as top
+    ///    10 5 + FizzBuzz return type Out15 as top
+    /// );
+    /// assert_eq!(Out1::eval(), 1);
+    /// assert_eq!(Out2::eval(), 2);
+    /// assert_eq!(Out3::eval(), true);
+    /// assert_eq!(Out4::eval(), 4);
+    /// assert_eq!(Out5::eval(), false);
+    /// assert_eq!(Out15::eval(), 0);
+    /// ```
+    iff,
+
+    #[doc(hidden)]
+    elsef,
+
+    #[doc(hidden)]
+    then,
+
+    /// Apply logical not to the top element
+    not,
+    /// Decrement the top element
+    pred, 
+    /// Index the fibonacci sequence with the top element
+    fib,
+    /// Calculate the factorial of the top element
+    fact, 
+    /// ( + ) Add two top elements togeth
+    plus, 
+    /// ( - ) Subtract the top element from the second top eleme
+    minus,
+    /// ( % ) Calculate the rest from dividing the second top element by the top o
+    modulo,
+    /// ( * ) Multiply two top elemen
+    mult, 
+    /// ( = ) Chech if two top elements are equ
+    eq,
+    /// ( < ) Check if the second top element is less than the top eleme
+    less,
+    /// Logical and two top elements
+    and,
+    /// Logical or two top elements
+    or,
+    /// ( 0 ) Constant number
+    zero,
+    /// ( 1 ) Constant number
+    one,
+    /// ( 2 ) Constant number
+    two,
+    /// ( 3 ) Constant number
+    three,
+    /// ( 4 ) Constant number
+    four,
+    /// ( 5 ) Constant number
+    five,
+    /// ( 6 ) Constant number
+    six,
+    /// ( 7 ) Constant number
+    seven,
+    /// ( 8 ) Constant number
+    eight,
+    /// ( 9 ) Constant number
+    nine,
+    /// ( 10 ) Constant number 
+    ten,
+    /// ( true ) Constant boolean
+    truef,
+    /// ( false ) Constant boolean
+    falsef
+);
+
 macro_rules! stack_op {
     (1, $name:ident, $op:ident, $type:ident) => {
-        pub_trait!($name);
         impl<V, N> $name for Node<V, N>
         where
             V: $op + $type,
@@ -34,7 +224,6 @@ macro_rules! stack_op {
         }
     };
     (2, $name:ident, $op:ident, $type:ident) => {
-        pub_trait!($name);
         impl<V, N> $name for Node<V, N>
         where
             N: drop + top,
@@ -62,7 +251,6 @@ stack_op!(2, or, OrElse, Bool);
 
 macro_rules! constant {
     ($name:ident, $con:ty) => {
-        pub_trait!($name);
         impl<V, N> $name for Node<V, N> {
             type Result = Node<$con, Self>;
         }
@@ -85,8 +273,6 @@ constant!(nine, Nine);
 constant!(ten, Ten);
 constant!(truef, True);
 constant!(falsef, False);
-
-pub_trait!(drop, dup, swap, rot, top, iff, elsef, then);
 
 impl<V, N> drop for Node<V, N> {
     type Result = N;
@@ -166,14 +352,20 @@ impl_for_stop!(
     five, six, seven, eight, nine, ten, truef, falsef, swap, rot, not, pred, fact, fib
 );
 
+/// Compile forth to trait expressions
 #[macro_export]
 macro_rules! forth {
     ({ $EX:ty }) => { };
     ({ $EX:ty } return) => {
         $EX
     };
-    ({ $EX:ty } return type $name:ident) => {
+    ({ $EX:ty } return type $name:ident as $tok:tt $($token:tt)*) => {
+        type $name = <$EX as $tok>::Result;
+        forth!({ $EX } $($token)*)
+    };
+    ({ $EX:ty } return type $name:ident $($token:tt)*) => {
         type $name = $EX;
+        forth!({ $EX } $($token)*)
     };
     ({ $EX:ty } . $($token:tt)*) => {
         println!("{}", <$EX as top>::Result::eval());
@@ -189,7 +381,9 @@ macro_rules! forth {
         pub trait $name {
             type Result;
         }
-        impl_for_stop!($name);
+        impl<N> $name for Stop<N> {
+            type Result = Self;
+        }
         forth!(@compile_impl $name; $($cmd)*);
         forth!({ $EX } $($token)*);
     };
